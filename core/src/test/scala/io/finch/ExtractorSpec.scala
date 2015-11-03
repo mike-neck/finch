a8c5a19bab779b4aa195aab9f97a6bb24cf06368
Vladimir Kostyukov <vkostyukov@twitter.com>
2015-11-03 09:34:13 -0800

    WIP

--- core/src/test/scala/io/finch/ExtractorSpec.scala
+++ core/src/test/scala/io/finch/ExtractorSpec.scala
@@ -7,32 +7,32 @@ import com.twitter.util.Try
 class ExtractorSpec extends FinchSpec {
 
   "An extracting Endpoint (string)" should "extract one path segment" in {
-    check { (i: Input) =>
-      awaitValue(string(i)) === i.headOption
+    check { i: Input =>
+      string(i).value === i.headOption
     }
   }
 
   "An extracting Endpoint (int)" should "extract one path segment" in {
     check { i: Input =>
-      awaitValue(int(i)) === i.headOption.flatMap(s => Try(s.toInt).toOption)
+      int(i).value === i.headOption.flatMap(s => Try(s.toInt).toOption)
     }
   }
 
   "An extracting Endpoint (boolean)" should "extract one path segment" in {
     check { i: Input =>
-      awaitValue(boolean(i)) === i.headOption.flatMap(s => Try(s.toBoolean).toOption)
+      boolean(i).value === i.headOption.flatMap(s => Try(s.toBoolean).toOption)
     }
   }
 
   "An extracting Endpoint (long)" should "extract one path segment" in {
     check { i: Input =>
-      awaitValue(long(i)) === i.headOption.flatMap(s => Try(s.toLong).toOption)
+      long(i).value === i.headOption.flatMap(s => Try(s.toLong).toOption)
     }
   }
 
   "An extracting Endpoint (uuid)" should "extract one path segment" in {
     check { i: Input =>
-      awaitValue(uuid(i)) === i.headOption.flatMap(s => Try(UUID.fromString(s)).toOption)
+      uuid(i).value === i.headOption.flatMap(s => Try(UUID.fromString(s)).toOption)
     }
   }
 }
